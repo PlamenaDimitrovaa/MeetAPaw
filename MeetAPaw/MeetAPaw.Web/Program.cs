75c1f5461a692211404f5450b531e742c3e5a03f
Plamena <plamena3456@abv.bg>
2023-07-31 21:45:47 +0300

    Added custom error pages.

--- MeetAPaw/MeetAPaw.Web/Program.cs
+++ MeetAPaw/MeetAPaw.Web/Program.cs
@@ -46,7 +46,6 @@ namespace MeetAPaw.Web
 
             WebApplication app = builder.Build();
 
-            // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
                 app.UseMigrationsEndPoint();
@@ -54,8 +53,8 @@ namespace MeetAPaw.Web
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseExceptionHandler("/Home/Error/500");
+                app.UseStatusCodePagesWithRedirects("/Home/Error?statusCode={0}");
                 app.UseHsts();
             }
 
